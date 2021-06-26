d732dd71805fbfdc2ceb0101418c7f22dce9501a
Manuel Sotelo Fernandez <msotelo-@c3r13s2.42madrid.com>
2021-06-26 19:28:27 +0200

    new functions 3

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: msotelo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 15:50:34 by msotelo-          #+#    #+#             */
-/*   Updated: 2021/06/26 16:52:58 by msotelo-         ###   ########.fr       */
+/*   Updated: 2021/06/26 19:28:13 by msotelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 char	*ft_strtrim(char const *s1, char const *set)
@@ -14,10 +14,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 	int		j;
 	int		k;
+	int		x;
 	char	*res;
 
 	i = ft_strlen(set);
 	j = ft_strlen(s1);
 	k = 0;
-	while (
+	while (s1[k] != '\0')
+	{
+		x = 0;
+		while (set[x] != '\0')
+		{
+			if (s1[k] == set[x])
+			{
+
+			}
+			else if (s1[j] == set[x])
+			{
+
+			}
+		}
+	}
 }
