771dd681576edec1922103b976b65b523487f88d
manuel <sotelomanuel66@gmail.com>
2021-06-30 10:02:47 +0200

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -8,7 +8,40 @@
 /*   Created: 2021/06/14 14:10:04 by msotelo-          #+#    #+#             */
 /*   Updated: 2021/06/14 14:58:04 by msotelo-         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */int	ft_atoi(const char *str)
+/* ************************************************************************** */
+int	ft_atoi(const char *str)
 {
+	int	sing;
+	int	i;
+	long	n;
 
+	sing = 1;
+	i = 0;
+	n = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+	{
+		i++;
+	}
+	if (str[i] != '+' && str[i] != '-' && ft_isdigit((int)str[i]) == 0)
+	{
+		return(0);
+	}
+	if (str[i] == '-')
+	{
+		sign = -1;
+	}
+	while (ft_isdigit((int)str[i]) == 1)
+	{
+		n = n * 10 + (str[i] - '0');
+		i++;
+		if ((n * sign) > 2147483647)
+		{
+			return (-1);
+		}
+		if ((n * sign) < -2147483648)
+		{
+			return (0);
+		}
+	}
+	return (n * sign);
 }
