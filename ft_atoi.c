c48e8b9489842af37388b2fdef5833b598552af7
Manuel Sotelo Fernandez <msotelo-@c3r13s2.42madrid.com>
2021-09-24 19:26:32 +0200

    arreglando atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: msotelo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 14:10:04 by msotelo-          #+#    #+#             */
-/*   Updated: 2021/09/24 17:03:46 by msotelo-         ###   ########.fr       */
+/*   Updated: 2021/09/24 19:26:09 by msotelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -21,11 +21,19 @@ int	ft_atoi(const char *str)
 	i = 0;
 	n = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+	{
+		if (str[i] == '+' || str[i] == '-')
+			if (str[i+1] == '+' || str[i+1]  == '-')
+				return (0);
 		i++;
+	}
 	if (str[i] != '+' && str[i] != '-' && ft_isdigit((int)str[i]) == 0)
 		return (0);
 	if (str[i] == '-')
+	{
 		sign = -1;
+		i++;
+	}
 	while (ft_isdigit((int)str[i]) == 1)
 	{
 		n = n * 10 + (str[i] - '0');
